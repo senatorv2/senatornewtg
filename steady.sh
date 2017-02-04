7c8fe1aac4fcbbf9353fe6e74e2c83c2cedb1b0f
senatorv2 <shpan@hi2.in>
2017-02-04 03:42:01 +0330

    Update steady.sh

--- steady.sh
+++ steady.sh
@@ -32,7 +32,7 @@ cat << EOF
  
 EOF
 echo -e "               \e[100m                Steady script            \e[00;37;40m"
-echo -e "               \e[01;34m          by not team              \e[00;37;40m"
+echo -e "               \e[01;34m          by senatortem              \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
